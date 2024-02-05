9a68879c50ff8cbcc8fc34ca0b33f29b7ef4853d
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2024-02-05 12:40:15 +0100

    vault backup: 2024-02-05 12:40:15

---
+++ Workspace/07-php-oop2/Live/index.php
@@ -0,0 +1,165 @@
+<?php
+
+    class User {
+
+        private $username;
+        private $email;
+        private $password;
+
+        public function __construct(
+            $username, $email, $password
+        ) {
+
+            $this -> setUsername($username);
+            $this -> setEmail($email);
+            $this -> setPassword($password);
+        }
+
+        public function getUsername() {
+
+            return $this -> username;
+        }
+        public function setUsername($username) {
+
+            if (!is_string($username) || strlen($username) < 3)
+                return;
+
+            $this -> username = $username;
+        }
+
+        public function getEmail() {
+
+            return $this -> email;
+        }
+        public function setEmail($email) {
+
+            $this -> email = $email;
+        }
+
+        public function getPassword() {
+
+            return $this -> password;
+        }
+        public function setPassword($password) {
+
+            $this -> password = $password;
+        }
+    }
+    class Employee extends User {
+
+        // COME SE...
+        // $username;
+        // $email;
+        // $password;
+
+        private $level;
+        
+        public function __construct(
+            $username, $email, $password, $level
+        ) {
+
+            $this -> setUsername($username);
+            $this -> setEmail($email);
+            $this -> setPassword($password);
+            
+            $this -> setLevel($level);
+        }
+
+        // COME SE...
+        // public function getUsername() {...}
+        // public function setUsername($username) {...}
+
+        public function getLevel() {
+
+            return $this -> level;
+        }
+        public function setLevel($level) {
+
+            $this -> level = $level;
+        }
+
+        public function doSomething() {
+
+            // $this -> username = "Marco"; // ERROR
+            $this -> setUsername("Marco");
+        }
+    }
+    class PremiumUser extends User {
+
+        private $membership;        
+
+        public function __construct(
+            $username, $email, $password, Membership $membership
+        ) {
+
+            $this -> setUsername($username);
+            $this -> setEmail($email);
+            $this -> setPassword($password);
+            
+            $this -> setMembership($membership);
+        }
+
+        public function getMembership() {
+
+            return $this -> membership;
+        }
+        public function setMembership($membership) {
+
+            $this -> membership = $membership;
+        }
+    }
+    class Membership {
+
+        private $expireDate;
+
+        public function __construct($expireDate) {
+
+            $this -> setExpireDate($expireDate);
+        }
+
+        public function getExpireDate() {
+
+            return $this -> expireDate;
+        }
+        public function setExpireDate($expireDate) {
+
+            $this -> expireDate = $expireDate;
+        }
+    }
+    
+    $user = new User("Guybrush", "guybrush@gmail.com", "miapws");
+    var_dump($user);
+
+    echo "<br><br>";
+
+    // $user -> setUsername("Piero");
+
+    echo "User: " . $user -> getUsername();
+    echo "<br>";
+    echo "Email: " . $user -> getEmail();
+    echo "<br>";
+    echo "Password: " . $user -> getPassword();
+
+    echo "<br><br>";
+
+    $employee = new Employee("Guybrush", "guybrush@gmail.com", "miapws", 4);
+    var_dump($employee);
+    echo "<br>";
+
+    $employee -> doSomething();
+    var_dump($employee);
+
+    echo "<br><br>";
+
+    $membership = new Membership("10/03/2024");
+
+    $premiumUser = new PremiumUser("Guybrush", "guybrush@gmail.com", "miapws", $membership);
+    var_dump($premiumUser);
+
+    $membership2 = $premiumUser -> getMembership();
+
+    echo "<br><br>";
+
+    echo "Username: " . $premiumUser -> getUsername();
+    echo "<br>";
+    echo "Expire date: " . $premiumUser -> getMembership() -> getExpireDate();
